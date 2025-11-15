37949cab1b8ff2658f9b2f379c519967809928ab
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2025-11-15 12:42:55 -0300

    feat: set default route

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -3,7 +3,7 @@ import { navigateToUrl } from "single-spa";
 export default function Root(props) {
    return (
     <nav>
-      <button onClick={() => navigateToUrl("/")}>Dashboard</button>
+      <button onClick={() => navigateToUrl("/dashboard")}>Dashboard</button>
       <button onClick={() => navigateToUrl("/transactions")}>Transações</button>
       <button onClick={() => navigateToUrl("/statement")}>Extrato</button>
     </nav>
