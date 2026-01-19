7569ebdfc5d8080e1c227e4e216059fa96f8a1de
Gabrielle-96 <gabrielle.martiins.96@gmail.com>
2026-01-19 18:40:24 -0300

    chore: remove date display and related code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,13 +22,3 @@ export const STORAGE_KEYS = {
   TOKEN: "token",
   USER_NAME: "userName",
 } as const;
-
-export const DAYS_OF_WEEK = [
-  "Domingo",
-  "Segunda-feira",
-  "Terça-feira",
-  "Quarta-feira",
-  "Quinta-feira",
-  "Sexta-feira",
-  "Sábado",
-] as const;
